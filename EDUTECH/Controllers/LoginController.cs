c943351646a5535f95f98e0a2eb34ec64697256b
Murad <Murad@DESKTOP-UMEK5DQ>
2023-11-21 15:58:56 +0300

    Add project files.

---
+++ EDUTECH/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using EDUTECH.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EDUTECH.Controllers
+{
+    public class LoginController : Controller
+    {
+        const string key = "UserId";
+        private readonly EDUTECHDBContext _context;
+
+        public LoginController(EDUTECHDBContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(User user)
+        {
+            var userNew = await _context.Users.Where(m => m.Email == user.Email && m.Password == user.Password).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                ModelState.AddModelError("Wrong", "Wrong username or password");
+                return View();
+            }
+            else if (user.IsAdmin == true)
+            {
+                //HttpContext.Session.SetInt32(key, user.Id);
+                return RedirectToAction("Index", "Home", new { area = "EmployeeDashbord" });
+            }
+            else if (user.IsSuperAgent == true)
+            {
+                //HttpContext.Session.SetInt32(key, user.Id);
+                return RedirectToAction("Index", "Admin",
+                        new { area = "Admin" });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EDUTECH/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EDUTECH.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsSuperAgent { get; set; }
+    }
+}
