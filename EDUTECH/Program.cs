2de063526ba8ea4c4978f1c5fa859c792f7cf159
Murad <Murad@DESKTOP-UMEK5DQ>
2023-11-22 03:36:18 +0300

    ...

--- EDUTECH/Program.cs
+++ EDUTECH/Program.cs
@@ -24,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=User}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
